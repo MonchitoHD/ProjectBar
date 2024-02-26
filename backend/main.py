848ca407a2494b9b59264a73c746a9191f2574b6
ItsHugoF <hugofuentesfern@gmail.com>
2024-02-26 17:43:45 +0100

    Primeros avances

--- backend/main.py
+++ backend/main.py
@@ -17,27 +17,27 @@ def conectar_mongodb():
     mesas = database.mesas
     mesas.delete_many({})
     array_mesas = [
-        {"tipo": "mesa", "asientos":2, "estado":"libre"},
-        {"tipo": "mesa", "asientos":3, "estado":"libre"},
-        {"tipo": "mesa", "asientos":4, "estado":"libre"},
-        {"tipo": "mesa", "asientos":5, "estado":"libre"},
-        {"tipo": "mesa", "asientos":6, "estado":"libre"}
+        {"numero_mesa":1,"tipo": "mesa", "asientos":2, "estado":"libre"},
+        {"numero_mesa":2,"tipo": "mesa", "asientos":3, "estado":"libre"},
+        {"numero_mesa":3,"tipo": "mesa", "asientos":4, "estado":"libre"},
+        {"numero_mesa":4,"tipo": "mesa", "asientos":5, "estado":"libre"},
+        {"numero_mesa":5,"tipo": "mesa", "asientos":6, "estado":"libre"}
     ]
     mesas.insert_many(array_mesas)
 
     #Una vez se realiza la conexion se crea la barra
     barra = database.barra
     barra.delete_many({})
-    barra_data = {"tipo": "barra", "asientos":5, "estado":"libre"}
+    barra_data = {"numero_mesa":6,"tipo": "barra", "asientos":5, "estado":"libre"}
     barra.insert_one(barra_data)
 
     #Una vez se realiza la conexion se crean las mesas de la terraza
     terraza = database.terraza
     terraza.delete_many({})
     mesas_terraza = [
-        {"tipo": "terraza", "asientos":2, "estado":"libre"},
-        {"tipo": "terraza", "asientos":4, "estado":"libre"},
-        {"tipo": "terraza", "asientos":6, "estado":"libre"}
+        {"numero_mesa":7,"tipo": "terraza", "asientos":2, "estado":"libre"},
+        {"numero_mesa":8,"tipo": "terraza", "asientos":4, "estado":"libre"},
+        {"numero_mesa":8,"tipo": "terraza", "asientos":6, "estado":"libre"}
     ]
     terraza.insert_many(mesas_terraza)
 
@@ -56,3 +56,83 @@ def conectar_mongodb():
 
 if __name__ == "__main__": 
     conectar_mongodb()
+
+#Funcion que muestra todas las mesas que hay en la BD con todos sus datos
+def mostrar_mesas():
+    global database
+    mesas = database.mesas.find()
+    mesas_barra = database.barra.find()
+    mesas_terraza = database.terraza.find()
+    for mesa in mesas:
+        print(mesa)
+    for mesa in mesas_barra:
+        print(mesa)    
+    for mesa in mesas_terraza:
+        print(mesa)
+
+
+#Menu para gestionar el funcionamiento de la aplicacion de terminal
+def mostrar_menu():
+    print("-------Menu--------")
+    print("1. Ver Mesas")
+    print("2. Reservar mesas")
+    print("3. Gestionar pedidos")
+    print("4. Liberar mesas")
+    print("5. Ver factura")
+
+
+#Menu para gestionar las mesas 
+def menu_mesas():
+    print("Opciones Mesas")
+    print("1. Ver Mesas")
+    print("2. Ver QR")
+
+def opcion1_mesas():
+    mostrar_mesas()
+
+def opcion2_mesas():
+    print("Hola Mundo") 
+
+def gestion_mesas():
+    menu_mesas()
+    opcion = input("Seleccione una opcion: ")
+
+    if opcion == "1":
+        opcion1_mesas()
+    elif opcion == "2":
+        opcion2_mesas()
+
+
+#Opciones del menu principal
+def opcion1():
+    gestion_mesas()
+
+def opcion2():
+    print("Hola mundo")
+
+def opcion3():
+    print("Hola mundo")
+
+def opcion4():
+    print("Hola mundo")
+
+def opcion5():
+    print("Hola mundo")
+
+while True:
+    mostrar_menu()
+    opcion = input("Seleccione una opcion: ")
+
+    if opcion == "1":
+        opcion1()
+    elif opcion == "2":
+        opcion2()
+    elif opcion == "3":
+        opcion3()
+    elif opcion == "4":
+        opcion4()
+    elif opcion == "5":
+        opcion5()
+    else:
+        print("La opcion que has seleccionado es incorrecta.")    
+
